899384379b60471b49f101fb21d6c4121d17c7d3
Oyvind Moldestad <moldestad@gmail.com>
2019-04-10 12:39:54 +0200

    Update phone number validator to allow spaces

--- src/validators/phone-number/phone-number-validator.test.ts
+++ src/validators/phone-number/phone-number-validator.test.ts
@@ -8,16 +8,22 @@ import {
 describe('phone-number-validator', () => {
   describe('isValidNorwegianPhoneNumber', () => {
     each`
-      phoneNumber       | expected
-      ${'81234567'}     | ${true}
-      ${'004781234567'} | ${true}
-      ${'+4781234567'}  | ${true}
-      ${'01234567'}     | ${false}
-      ${'8123456'}      | ${false}
-      ${'812345678'}    | ${false}
-      ${'8123456a'}     | ${false}
-      ${undefined}      | ${false}
-      ${null}           | ${false}
+      phoneNumber           | expected
+      ${'81234567'}         | ${true}
+      ${'81 23 45 67'}      | ${true}
+      ${'812 34 567'}       | ${true}
+      ${'004781234567'}     | ${true}
+      ${'0047 81 23 45 67'} | ${true}
+      ${'004781234567'}     | ${true}
+      ${'+4781234567'}      | ${true}
+      ${'01234567'}         | ${false}
+      ${'01 23 45 67'}      | ${false}
+      ${'8123456'}          | ${false}
+      ${'812345678'}        | ${false}
+      ${'812 34 5678'}      | ${false}
+      ${'8123456a'}         | ${false}
+      ${undefined}          | ${false}
+      ${null}               | ${false}
     `.it(
       'should return $expected for $phoneNumber',
       ({ phoneNumber, expected }) => {
@@ -30,11 +36,15 @@ describe('phone-number-validator', () => {
     each`
       phoneNumber        | expected
       ${'41234567'}      | ${true}
+      ${'41 23 45 67'}   | ${true}
       ${'91234567'}      | ${true}
+      ${'912 34 567'}    | ${true}
       ${'+4741234567'}   | ${true}
       ${'004741234567'}  | ${true}
       ${'81234567'}      | ${false}
       ${'4123456'}       | ${false}
+      ${'41 23 456'}     | ${false}
+      ${'412 34 5678'}   | ${false}
       ${'412345678'}     | ${false}
       ${'8123456a'}      | ${false}
       ${undefined}       | ${false}
--- src/validators/phone-number/phone-number-validator.ts
+++ src/validators/phone-number/phone-number-validator.ts
@@ -1,5 +1,5 @@
 export const isValidNorwegianPhoneNumber = (phoneNumber: string): boolean =>
-  /^((\+|00)47){0,1}[1-9]\d{7}$/.test(phoneNumber);
+  /^((\+|00)47){0,1}\s{0,1}[1-9](\s{0,1}\d){7}$/.test(phoneNumber);
 
 export const isValidNorwegianCellPhoneNumber = (phoneNumber: string): boolean =>
-  /^((\+|00)47){0,1}(4|9)\d{7}$/.test(phoneNumber);
+  /^((\+|00)47){0,1}\s{0,1}(4|9)(\s{0,1}\d){7}$/.test(phoneNumber);
