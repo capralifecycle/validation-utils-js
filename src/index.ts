e4d178edb832c17e60c71e1fdc0edc2cb5c606d5
Oyvind Moldestad <moldestad@gmail.com>
2019-04-10 13:34:16 +0200

    Export common validators

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './validators/common/common-validator';
 export * from './validators/email/email-validator';
 export * from './validators/phone-number/phone-number-validator';
 export * from './validators/zip-code/zip-code-validator';
