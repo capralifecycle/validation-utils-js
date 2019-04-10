47a6906a6a77f387f760652cf3f23a9ade7d7e36
Oyvind Moldestad <moldestad@gmail.com>
2019-04-10 12:57:23 +0200

    Update publish script

--- publish.sh
+++ publish.sh
@@ -2,13 +2,15 @@
 
 VERSION=$1
 
-if [[ $VERSION != "major" ] && [ $VERSION != "minor"] && [ $VERSION != "patch"]]; then
+shopt -s extglob
+if [[ $VERSION == @(major|minor|patch) ]]; then
+    echo "Pushing tags and publishing package"
+    npm version $VERSION && \
+        git push && \
+        git push --tags && \
+        npm publish --access public
+else
     echo "Missing version statement. Select one of <patch | minor | major>"
     exit 1
 fi
 
-echo "Pushing tags and publishing package"
-npm version $VERSION && \
-    git push && \
-    git push --tags && \
-    npm publish --access public
