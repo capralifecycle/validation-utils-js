e24dde4a956eb6e63ce2752cd2f3fbfd71b9b073
thrap <magnus.solheim@gmail.com>
2019-10-01 13:02:59 +0200

    add mod 11 function

--- src/validators/common/common-validator.ts
+++ src/validators/common/common-validator.ts
@@ -1,2 +1,10 @@
 export const isNonEmptyString = (value: string): boolean =>
   !!value && value.constructor === String;
+
+// https://no.wikipedia.org/wiki/MOD11
+export const mod11 = (value: string): number => {
+	const sum = value.split('').reverse().reduce((acc, x, i) =>
+		acc + parseInt(x) * [2,3,4,5,6,7][i%6]
+	, 0)
+	return 11 - sum % 11;
+}
