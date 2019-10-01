8ce1f68c77c976093b8942b498970bc492fdf7d1
thrap <magnus.solheim@gmail.com>
2019-10-01 15:20:04 +0200

    make mod11 more generic

--- src/validators/common/common-validator.ts
+++ src/validators/common/common-validator.ts
@@ -2,9 +2,9 @@ export const isNonEmptyString = (value: string): boolean =>
   !!value && value.constructor === String;
 
 // https://no.wikipedia.org/wiki/MOD11
-export const mod11 = (value: string): number => {
+export const mod11 = (value: string, factors: number[] = [2,3,4,5,6,7]): number => {
 	const sum = value.split('').reverse().reduce((acc, x, i) =>
-		acc + parseInt(x) * [2,3,4,5,6,7][i%6]
+		acc + parseInt(x) * factors[i%factors.length]
 	, 0)
-	return 11 - sum % 11;
+	return (11 - sum % 11) % 11;
 }
