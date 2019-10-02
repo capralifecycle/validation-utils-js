9a4fe252217cb9c3db0b3c04a8f64829d1cc805d
thrap <magnus.solheim@gmail.com>
2019-10-02 08:55:03 +0200

    formatting

--- src/validators/account-number/account-number-validator.ts
+++ src/validators/account-number/account-number-validator.ts
@@ -1,6 +1,6 @@
 import { mod11 } from '../common/common-validator';
 
 export const isValidAccountNumber = (accountNumber: string): boolean => {
-  	const trimmed = ('' + accountNumber).replace(/\./g, '').trim();
-  	return /^\d{11}$/.test(trimmed) && mod11(trimmed.substr(0, 10)) === parseInt(trimmed.substr(-1),10);
+	const trimmed = ('' + accountNumber).replace(/\./g, '').trim();
+	return /^\d{11}$/.test(trimmed) && mod11(trimmed.substr(0, 10)) === parseInt(trimmed.substr(-1),10);
 }
--- src/validators/organization-number/organization-number-validator.ts
+++ src/validators/organization-number/organization-number-validator.ts
@@ -2,6 +2,6 @@ import { mod11 } from '../common/common-validator';
 
 // https://www.brreg.no/om-oss/oppgavene-vare/alle-registrene-vare/om-enhetsregisteret/organisasjonsnummeret/
 export const isValidOrganizationNumber = (OrganizationNumber: string): boolean => {
-  	const trimmed = ('' + OrganizationNumber).replace(/\./g, '').trim();
-  	return /^\d{9}$/.test(trimmed) && mod11(trimmed.substr(0, 8)) === parseInt(trimmed.substr(-1),10);
+	const trimmed = ('' + OrganizationNumber).replace(/\./g, '').trim();
+	return /^\d{9}$/.test(trimmed) && mod11(trimmed.substr(0, 8)) === parseInt(trimmed.substr(-1),10);
 }
