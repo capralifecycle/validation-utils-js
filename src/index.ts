8b2c97b62f327611594850f828af70d7f815c5bd
thrap <magnus.solheim@gmail.com>
2020-04-17 13:25:15 +0200

    fix(validators): export organization, account and national identification number validators

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './validators/common/common-validator';
 export * from './validators/email/email-validator';
 export * from './validators/phone-number/phone-number-validator';
 export * from './validators/zip-code/zip-code-validator';
+export * from './validators/account-number/account-number-validator';
+export * from './validators/organization-number/organization-number-validator';
+export * from './validators/national-identification-number/national-identification-number-validator';
